7bb08260ca5fef885f683cf29e01189b42a7c973
6C1 <cooper@cooperstimson.com>
2015-05-25 12:27:50 +0000

    Adding test for integer data

--- tests/bloompy_test.py
+++ tests/bloompy_test.py
@@ -2,6 +2,7 @@ from nose import *
 from bloompy.bloom import *
 from random import choice as rc
 from string import printable as ascii
+from string import digits
 
 def basic_test():
     k = 16180
@@ -12,4 +13,12 @@ def basic_test():
         b.store(s)
         assert s in b
 
-        
+def int_test():
+    k = 16180
+    
+    b = BloomFilter()
+    for i in xrange(k):
+        s = int("".join([rc(digits) for j in xrange(64)]))
+        b.store(s)
+        assert s in b
+    
